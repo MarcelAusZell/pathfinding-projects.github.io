575bde802205c4eabeff58790b97a483303087f4
Marcel Sayegh <marcel.sayegh@gmx.de>
2025-06-12 17:21:13 +0200

    Changed base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",
+  base: "/pathfinding-projects.github.io/",
   plugins: [tailwindcss(), react()],
   build: {
     outDir: "docs"
